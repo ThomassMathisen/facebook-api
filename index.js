2fcc85579beb8501e1faecf3393d6c5924893e12
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-10 14:30:49 +0100

    Setup User model and login/register routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const helmet = require("helmet")
+const userRouter = require("./routes/users")
+const authRouter = require("./routes/auth")
+const postRouter = require("./routes/posts")
 
 dotenv.config()
 
@@ -22,6 +25,10 @@ app.use(morgan("common"))
 app.get("/", (req, res) => {
   res.send("Welcome")
 })
+
+app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/posts", postRouter)
 
 app.listen(8800, () => {
   console.log("Backend is running")
